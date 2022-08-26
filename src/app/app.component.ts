609eda2a97d422db806efba4733119770e8e6115
Phongsakorn <106302862+PhongsakornK@users.noreply.github.com>
2022-08-26 14:12:08 +0700

    basic ui 6th

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'AngBi2';
+  is_selected:any=[]
+  constructor()
+  {
+
+  }
+  ngOnInit()
+  {
+    if(this.is_selected)
+    {
+      alert(this.is_selected[0].value)
+    }
+  }
 }
