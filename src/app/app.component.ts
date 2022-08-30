0aceea99aaf35a46585a484fedce61f6b9953040
Phongsakorn <106302862+PhongsakornK@users.noreply.github.com>
2022-08-30 16:34:05 +0700

    add Mockup1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartComponent } from "ng-apexcharts";
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,18 +7,69 @@ import { ChartComponent } from "ng-apexcharts";
 })
 export class AppComponent implements OnInit{
   title = 'AngBi2';
-  is_selected:any=[]
-
-  constructor()
-  {
-
-  }
-
-  ngOnInit()
-  {
-    if(this.is_selected)
+  mock_tbl_1 = [
     {
-      alert(this.is_selected[0].value)
+      shareholders:6747,peoples:1234,legal_person:24,normal_people:3459,total_course:59
     }
+  ]
+  mock_tbl_2 = [
+    {
+      activity_name:"Thaitrad",attendee:305,
+    },
+    {
+      activity_name:"activiOnline Business Matching",attendee:23
+    },
+    {
+      activity_name:"งานแสดงสินค้าในประเทศ",attendee:1
+    },
+    {
+      activity_name:"งานแสดงสินค้าต่างประเทศ",attendee:28
+    },
+    {
+      activity_name:"SMEs Proactive",attendee:9
+    },
+  ]
+
+  mock_tbl_5 = [
+    {
+      countries:"Thailand",attendee:999,
+    },
+    {
+      countries:"The United States of America",attendee:999
+    },
+    {
+      countries:"China",attendee:777
+    },
+    {
+      countries:"Japan",attendee:777
+    },
+    {
+      countries:"Japan",attendee:777
+    },
+  ]
+  mock_tbl_6 = [
+    {
+      countries:"Thailand",values:123,
+    },
+    {
+      countries:"The United States of America",attendee:120
+    },
+    {
+      countries:"China",values:119
+    },
+    {
+      countries:"Japan",values:118
+    },
+    {
+      countries:"Korea",values:117
+    },
+  ]
+  is_selected:any=[]
+  resp:any
+  constructor(private httpR:HttpClient){}
+  ngOnInit(): void
+  {
   }
+
+
 }
